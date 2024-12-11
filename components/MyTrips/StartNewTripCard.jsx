b8a18d59680aed7b574e21a0b6e0e1f17586eb34
aravindkk004 <aravind463kk30@gmail.com>
2024-12-11 08:39:04 +0530

    final

--- components/MyTrips/StartNewTripCard.jsx
+++ components/MyTrips/StartNewTripCard.jsx
@@ -2,8 +2,10 @@ import { View, Text, TouchableOpacity } from "react-native";
 import React from "react";
 import Ionicons from "@expo/vector-icons/Ionicons";
 import { Colors } from "@/constants/Colors";
+import { useRouter } from "expo-router";
 
 export default function StartNewTripCard() {
+  const router = useRouter();
   return (
     <View
       style={{
@@ -26,6 +28,7 @@ export default function StartNewTripCard() {
       </Text>
 
       <TouchableOpacity
+        onPress={() => router.push("/create-trip/search-place")}
         style={{
           padding: 15,
           backgroundColor: Colors.PRIMARY,
